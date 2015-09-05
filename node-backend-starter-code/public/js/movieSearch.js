a2bb82cfe720e88132c5410de2a92c5970244203
James Naadjie <james.naadjie@gmail.com>
2015-09-04 23:29:38 -0400

    refactor to use DOM object methods

--- node-backend-starter-code/public/js/movieSearch.js
+++ node-backend-starter-code/public/js/movieSearch.js
@@ -1,93 +1,47 @@
-MOVIES = [{title: 'Batman'}];
-
-
-var searchForm = $('form');
-var container = $('#container');
-container.hide();
-searchForm.on('submit', function(event){
-    event.preventDefault();
-    var movieTitle = searchForm.find('input').val(); //query parameter
+var searchForm = document.getElementById("search");
+searchForm.addEventListener("submit",function(e){
+    e.preventDefault();
+    document.getElementById("results").innerHTML = "";
+    var movieTitle = document.getElementById("query-string").value;
     var url = 'http://www.omdbapi.com/?s=' + movieTitle;
     $.ajax(url, {
         complete: function(data, status){
             console.log(status);
             var results = $.parseJSON(data.responseText);
-            console.log(results['Search']);
-            results['Search'].forEach(function(movie){
-                getMovieDetails(movie.Title);
-                container.find('table tbody').append("<tr><td>movie.Title</td><td>{{movie.year}}</td></tr>");
-//                    $Container.find('.title').text(movie.Title);
-//                    $Container.find('.plot').text(movie.Plot);
-//                    $Container.find('.poster').html('<img src="' + movie.Poster + '"/>');
-//                    $Container.find('.year').text(movie.Year);
-            });
-            container.show();
+            results['Search'].forEach(createList);
         }
     });
 });
 
+function createList(movie){
+    var fields = [movie.Title, movie.Year];
+    var result = document.createElement("div");
+    var list = document.createElement('ul');
+
+    result.setAttribute("id", movie.Title);
+    result.addEventListener("click", function(e){
+        e.preventDefault();
+        getMovieDetails(this.id);
+    });
+
+    fields.forEach(function(field){
+        var item = document.createElement('li');
+        item.appendChild(document.createTextNode(field));
+        list.appendChild(item);
+    });
+    result.appendChild(list);
+    return document.getElementById("results").appendChild(result);
+}
+
 function reqListener () {
-    console.log(this.responseText);
-    $("#results").append(this.responseText);
+    var movieDetails = JSON.parse(this.responseText);
+    console.log(movieDetails);
+    document.getElementById("movie-details").innerHTML = movieDetails.Title;
 }
 
 function getMovieDetails(movietitle) {
     var oReq = new XMLHttpRequest();
     oReq.addEventListener("load", reqListener);
-    console.log(movietitle);
     oReq.open("GET", "http://www.omdbapi.com/?t=" + movietitle + "&r=json", true);
     oReq.send();
 }
-
-
-//$('#dynamictable').append('<table></table>');
-//var table = $('#dynamictable').children();
-//table.append("<tr><td>a</td><td>b</td></tr>");
-//table.append("<tr><td>c</td><td>d</td></tr>");
-
-//function MakeTablejQuery(rows, cols) {
-//    var table = $("<table/>").attr("border", 1);
-//    for (r = 0; r < rows; r++) {
-//        var row = $("<tr/>");
-//        for (var c = 0; c < cols; c++) {
-//            if (r == 0) {
-//                row.append($("<th/>").text("Column " + c))
-//            } else {
-//                row.append($("<td/>").text(c.toString() + r.toString()))
-//            }
-//        }
-//        table.append(row);
-//    }
-//    var container = $("#container")
-//    container.html('');
-//    container.append(table);
-//}
-//
-//MakeTablejQuery(5, 3);
-
-//functional style
-//Number.prototype.times = function(fn) {
-//    for(var r = [], i = 0; i < this; i++)
-//        r.push(fn(i));
-//    return r;
-//};
-//
-//function MakeTablejQuery(numRows, numCols) {
-//
-//    var header = numCols.times(function(c) {
-//        return $("<th/>").text("Column " + c);
-//    });
-//
-//    var row = function(r) {
-//        return $("<tr/>").append(numCols.times(function(c) {
-//            return $("<td/>").text([c, r].join(""));
-//        }));
-//    };
-//
-//    return $("<table/>")
-//        .append(header)
-//        .append(numRows.times(row))
-//        .attr("border", 1);
-//}
-//
-//$("#placeholder").append(MakeTablejQuery(5, 3));
