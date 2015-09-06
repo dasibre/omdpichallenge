bc27c66ec5db2ba9cc7f14669f10bfb1931b2ce1
James Naadjie <james.naadjie@gmail.com>
2015-09-06 15:24:21 -0400

    Refactor ajax calls

--- node-backend-starter-code/public/js/movieSearch.js
+++ node-backend-starter-code/public/js/movieSearch.js
@@ -1,6 +1,10 @@
+/**
+ * Created by jnaadjie on 9/6/15.
+ */
 (function(){
 
-    var url = "https://www.omdbapi.com/"; //using https to make heroku happy
+    var omdbapiUrl = "https://www.omdbapi.com/"; //using https to make heroku happy
+
     var movieSelectors = {
       overview: document.getElementById("movie-overview-info"),
       infoBar: document.getElementsByClassName("movie-info-bar"),
@@ -8,43 +12,43 @@
       title: document.getElementById("query-string"),
       results: document.getElementById("results"),
       loader: document.getElementById("loading"),
-      favorite: document.getElementById("favorite")
+      favorite: document.getElementById("favorite"),
+      favorites: document.getElementById("list-favorites")
     };
+
+    movieSelectors.favorites.addEventListener("click", showFavorites);
 
     //Add submit event listener, to get movies on submission
     movieSelectors.searchForm.addEventListener("submit",function(e){
         e.preventDefault();
+        loader.show();
         movieSelectors.results.innerHTML = "";
         var movieTitle = movieSelectors.title.value;
         var searchParam = '?s=' + movieTitle;
-        $.ajax(url + searchParam, {
-            complete: function(data, status){
-                if(!status === "success") {
-                    alert("Something Went wrong");
-                } else {
-                    var results = $.parseJSON(data.responseText);
-                    results['Search'].forEach(createList);
-                }
-            }
+        ajaxClient.get(omdbapiUrl + searchParam,function(){
+            loader.hide();
+            var results = JSON.parse(this.responseText);
+            results['Search'].forEach(createList);
         });
     });
 
-
-
-    //TODO Add function
-    //function searchByTitle(title) {
-    //    var searchParam = '?s=' + movieTitle;
-    //}
-
+    //Saves movie to favorites
     function favoriteAmovie(movie) {
-        var client = new XMLHttpRequest();
-        client.open("POST", "/favorites", true);
-        client.setRequestHeader("Content-Type", "application/json");
-        client.send(JSON.stringify(movie));
-        console.log(movie);
+        movie.favorites = "true";
+        ajaxClient.post("/favorites", JSON.stringify(movie));
     }
 
+    //displays all movies added to favorites
+    function showFavorites() {
+        movieSelectors.results.innerHTML = "";
+        movieSelectors.favorite.innerText = "Saved to Favorites";
+        ajaxClient.get("/favorites",function(){
+            var results = JSON.parse(this.responseText);
+            results.forEach(createList);
+        });
+    }
 
+    //creates an unordered list of divs using the results of index search
     function createList(movie){
         var fields = [movie.Title, movie.Year];
         var result = document.createElement("div");
@@ -54,8 +58,9 @@
         result.addEventListener("click", function(e){
             e.preventDefault();
             loader.show();
+            movieSelectors.favorite.innerText = (movie.favorite === "true") ? "Saved to favorites" : "favorite this movie";
             loading.style.display = "block";
-            getMovieDetails(this.id);
+            searchByTitle(this.id);
         });
 
         fields.forEach(function(field){
@@ -67,18 +72,39 @@
         return movieSelectors.results.appendChild(result);
     }
 
-    function toggleMovieInfoDisplay(cb) {
-        el = document.getElementById("overlay");
-        el.style.visibility = (el.style.visibility === "visible") ? "hidden" : "visible";
-        cb(el);
+    //Search Omdbapi by movie title
+    function searchByTitle(title) {
+        ajaxClient.get(omdbapiUrl + "?t=" + title + "&r=json",displayMovieInfo)
     }
 
+    //Display movie details
+    function displayMovieInfo () {
+        movie = JSON.parse(this.responseText);
+        setMovieFields(movie);
+        loader.hide();
+        toggleMovieInfoDisplay(function(element){
+            el = document.getElementById("close-modal");
+            el.addEventListener("click", function(e){
+                element.style.visibility = "hidden";
+            })
+        });
+    }
+
+    //Helper to toggle modal overlay
+    function toggleMovieInfoDisplay(cb) {
+        overlay = document.getElementById("overlay");
+        overlay.style.visibility = (overlay.style.visibility === "visible") ? "hidden" : "visible";
+        cb(overlay);
+    }
+
+    //Helper to set multiple element attributes
     function setAttributes(element, attrs) {
         for(var key in attrs) {
             element.setAttribute(key, attrs[key]);
         }
     }
 
+    //Set Movie fields for displaying movie details
     function setMovieFields(movieObj) {
         var movieInfoBar = movieSelectors.infoBar[0];
         var poster = movieSelectors.overview.getElementsByTagName("img")[0];
@@ -96,33 +122,38 @@
         movieSelectors.favorite.addEventListener("click", function(e) {
             favoriteAmovie(movieObj);
             this.removeEventListener('click', arguments.callee, false);
+            this.innerText = "Saved to favorites";
         },false);
     }
 
-    function reqListener () {
-        movie = JSON.parse(this.responseText);
-        setMovieFields(movie);
-        loader.hide();
-        toggleMovieInfoDisplay(function(element){
-            el = document.getElementById("close-modal");
-            el.addEventListener("click", function(e){
-                element.style.visibility = "hidden";
-            })
-        });
-    }
+    //Ajax client to get and post requests
+    var ajaxClient = {
+      get: function(url,reqListener) {
+          var client = new XMLHttpRequest();
+          client.ontimeout = function() {
+              alert("The request for " + omdbapiUrl + " timed out");
+              loader.hide();
+          };
+          client.addEventListener("load", reqListener);
+          client.open("GET", url, true);
+          client.timeout = 6000;
+          client.send();
+      },
 
-    function getMovieDetails(movietitle) {
-        var oReq = new XMLHttpRequest();
-        oReq.ontimeout = function() {
-            alert("The request for " + url + " timed out");
-            loader.hide();
-        };
-        oReq.addEventListener("load", reqListener);
-        oReq.open("GET", url + "?t=" + movietitle + "&r=json", true);
-        oReq.timeout = 3000;
-        oReq.send();
-    }
+      post: function(url,data) {
+          var client = new XMLHttpRequest();
+          client.ontimeout = function() {
+              alert("The request for " + omdbapiUrl + " timed out");
+              loader.hide();
+          };
+          client.open("POST", url, true);
+          client.setRequestHeader("Content-Type", "application/json");
+          client.timeout = 6000;
+          client.send(data);
+      }
+    };
 
+    //Turns loader gif on and off
     var loader = {
         show: function() {
             movieSelectors.loader.style.display = "block";
